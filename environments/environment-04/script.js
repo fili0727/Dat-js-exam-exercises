e0912d4468a315108843087c608408680526ac45
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-01 16:25:10 +0200

    done

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+// 1. Importer `teachers`-listen i `script.js`.
+function start() {
+  addTeacher("Peter", "petl@kea.dk");
+  teachers.forEach(displayTeachers);
+}
+// 2. Lav en funktion, der viser listen af alle `teacher`-objekter på websiden.
+
+function displayTeachers(teacher) {
+  const HTML = /*HTML*/ `
+ <li>Name: ${teacher.name}, Email: ${teacher.email}</li> `;
+  document
+    .querySelector("#teachers-list")
+    .insertAdjacentHTML("beforeend", HTML);
+}
+// 3. Lav en funktion der tilføjer et nyt `teacher`-objekt til listen.
+//Sørg for at nye `teacher`-objekter vises på websiden.
+function addTeacher(name, email) {
+  const newTeacher = {
+    name: name,
+    email: email,
+  };
+  teachers.push(newTeacher);
+}
