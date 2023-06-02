e44f594e2f657f48434730ebbba27886898bc9a6
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-02 12:51:27 +0200

    opgave 12 hehe ups

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+import { courses } from "./courses.js";
+// 1. Importer `courses`-listen i `script.js`.
+
+window.addEventListener("load", start);
+
+function start() {
+  showCourses();
+}
+// 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name`, `ectsPoints` og `teacher`.
+function showCourses() {
+  sortByPoints();
+  courses.forEach(showCourse);
+}
+
+function showCourse(course) {
+  const html = /*HTML*/ `
+  <li>${course.name},${course.ectsPoints},${course.teacher} </li>
+  `;
+
+  document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+}
+// 3. Lav en funktion, der sorterer listen af `courses` efter stigende antal ECTS. Vis den sorterde liste på websiden.
+function sortByPoints() {
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
