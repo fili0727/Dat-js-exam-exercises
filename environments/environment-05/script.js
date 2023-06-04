3198f061819054db170e1303ab0f07abd919634b
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-04 11:09:14 +0200

    opgave 13

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,21 +1,21 @@
 "use strict";
 
 import { courses } from "./courses.js";
+
 // 1. Importér `courses`-listen i `script.js`.
 window.addEventListener("load", start);
 
 function start() {
   document
     .querySelector("#select-filter-ects")
-    .addEventListener("change", selected);
-  showCourses();
+    .addEventListener("change", pointsSelected);
+  showCourses(courses);
 }
 // 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden.
 //Vis som minimum `name` og `ectsPoints`.
-function showCourses() {
-  console.log(courses);
+function showCourses(list) {
   document.querySelector("#courses-list").innerHTML = "";
-  courses.forEach(showCourse);
+  list.forEach(showCourse);
 }
 
 function showCourse(course) {
@@ -27,16 +27,9 @@ function showCourse(course) {
 // 3. Lav en funktion, der filtrer listen af `courses` på baggrund af valgte `option` i `select` (se `environment-05`).
 //Filtreringen ændrer sig hver gang en ny `option` vælges.
 
-function selected(event) {
-  console.log(event.target.value);
-  if (event.target.value == 5) {
-    courses.filter(a => a.ectsPoints == 5);
-  } else if (event.target.value == "10") {
-    courses.filter(a => a.ectsPoints == 10);
-  } else if (event.target.value == 15) {
-    courses.filter(a => a.ectsPoints == 15);
-  } else if (event.target.value == 20) {
-    courses.filter(a => a.ectsPoints == 20);
-  }
-  showCourses();
+function pointsSelected(event) {
+  showCourses(filterByPoints(courses, Number(event.target.value)));
+}
+function filterByPoints(list, points) {
+  return list.filter(a => a.ectsPoints === points);
 }
