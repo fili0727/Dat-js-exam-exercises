3eb7de2eacc4b6a3baf9137910b73280cc040147
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-04 11:48:10 +0200

    opgave 14

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// 1. Importér `courses`-listen i `script.js`.
+import { courses } from "./courses.js";
+// 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden.
+//Vis som minimum `name`, `startDate` `ectsPoints`.
+window.addEventListener("load", start);
+
+function start() {
+  showCourses(courses);
+}
+
+function showCourses(list) {
+  console.log(list);
+  document.querySelector("#courses-list").innerHTML = "";
+  sortByStartDate(list);
+  list.forEach(showCourse);
+}
+
+function showCourse(course) {
+  const html = /*HTML*/ ` 
+<li>${course.name}, ${course.ectsPoints}, ${course.startDate}</li>
+`;
+  document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+}
+// 3. Lav en funktion, der sorterer listen af `courses` stigende efter `startDate`.
+//Vis sorteringen på websiden.
+function sortByStartDate(list) {
+  list.sort((a, b) => a.startDate.localeCompare(b.startDate));
+  return list;
+}
