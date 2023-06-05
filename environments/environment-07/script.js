25279c58d3950971351462957f953da61286bd21
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-05 11:21:23 +0200

    opgave 20

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,5 +1,56 @@
 "use strict";
 
-// 1. Lav en funktion der opretter et `student` objekt med `name`, `email` og `age`, fra formularen på websiden og tilføjer det til en liste.
+window.addEventListener("load", start);
+const students = [];
+function start() {
+  document
+    .querySelector("#create-student-form")
+    .addEventListener("submit", submitClicked);
+  showStudents(students);
+}
+
+// 1. Lav en funktion der opretter et `student` objekt med `name`, `email`
+// og `age`, fra formularen på websiden og tilføjer det til en liste.
+function addStudent(name, email, age) {
+  const newStudent = { name: name, email: email, age: age };
+
+  students.push(newStudent);
+  console.log(students);
+  showStudents(students);
+}
+
+function submitClicked(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = form.age.value;
+  form.reset();
+  addStudent(name, email, age);
+}
+
+function showStudents(list) {
+  sortByAge();
+  document.querySelector("#students-table-body").innerHTML = "";
+  console.log(list);
+  list.forEach(showStudent);
+}
+
+function showStudent(student) {
+  const html = /*HTML*/ `
+  <tr>
+    <td>${student.name}</td>
+    <td>${student.email}</td>
+    <td>${student.age}</td>
+  </tr>
+  `;
+  document
+    .querySelector("#students-table-body")
+    .insertAdjacentHTML("beforeend", html);
+}
+
 // 2. Udskriv listen til websiden hver gang der bliver tilføjet en ny student.
 // 3. Sortér listen efter `age` så de yngste vises først.
+function sortByAge() {
+  students.sort((a, b) => a.age - b.age);
+}
