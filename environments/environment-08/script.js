27b25486af40637cd8fe036837b32f7e21a338fd
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-06-05 13:21:36 +0200

    øvelse 22

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let playlist;
+async function start() {
+  playlist = await getData();
+  showPlaylist();
+}
+
+async function getData() {
+  const response = await fetch("playlist.json");
+  const data = response.json();
+  return data;
+}
+
+function showPlaylist() {
+  document.querySelector("#songlist").innerHTML = "";
+  playlist.forEach(showSong);
+}
+function showSong(song, id) {
+  console.log(id);
+  const html = /*HTML*/ `
+  <li>${song.artist}: ${song.title} (${song.duration}) <button>-</button></li>
+  `;
+  document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+  document
+    .querySelector("#songlist li:last-child button")
+    .addEventListener("click", () => removeSong(id));
+}
+
+function removeSong(id) {
+  console.log(id);
+  playlist.splice(id, 1);
+  showPlaylist();
+}
+// 1. Lav en funktion der indlæser JSON-filen `playlist.json` og gemmer listen i en variabel.
+// 2. Lav en funktion til at udskrive listen af sange på websiden- tilføj en `remove`-knap til hver sang.
+// 3. Få remove-knappen til at fjerne den pågældende sang fra listen, og udskriv listen igen.
